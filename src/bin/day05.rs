0b2cccc88f6a3257fc98b4c17d59f457c30e9def
Pieter Bruyninckx <pbruyninckx@gmail.com>
2024-12-23 13:49:22 +0100

    Day 5 part 2

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -1,17 +1,19 @@
 use anyhow::Error;
 use itertools::Itertools;
-use std::collections::HashMap;
+use std::cmp::Ordering;
+use std::collections::{HashMap, HashSet};
 use std::fs::read_to_string;
 use std::path::Path;
 
 fn main() -> Result<(), Error> {
     let input = parse_input(&read_to_string(Path::new("data/input05.txt"))?);
-    println!("{}", solve(&input));
+    let (solution1, solution2) = solve(&input);
+    println!("{}\n{}", solution1, solution2);
     Ok(())
 }
 
 struct Input {
-    rules: Vec<(i32, i32)>,
+    rules: HashSet<(i32, i32)>,
     updates: Vec<Vec<i32>>,
 }
 
@@ -37,7 +39,7 @@ fn parse_input(input: &str) -> Input {
     Input { rules, updates }
 }
 
-fn valid_ordering(rules: &[(i32, i32)], update: &[i32]) -> bool {
+fn valid_ordering(rules: &HashSet<(i32, i32)>, update: &[i32]) -> bool {
     let page_position: HashMap<_, _> = update.iter().enumerate().map(|(i, &v)| (v, i)).collect();
     rules.iter().all(|&(page1, page2)| {
         let pos1 = page_position.get(&page1);
@@ -46,11 +48,28 @@ fn valid_ordering(rules: &[(i32, i32)], update: &[i32]) -> bool {
     })
 }
 
-fn solve(input: &Input) -> i32 {
+fn middle_update(rules: &HashSet<(i32, i32)>, updates: &[i32]) -> i32 {
+    let mut sorted_updates = updates.to_vec();
+    sorted_updates.sort_by(|&a, &b| {
+        if rules.contains(&(a, b)) {
+            Ordering::Less
+        } else {
+            Ordering::Greater
+        }
+    });
+    sorted_updates[sorted_updates.len() / 2]
+}
+
+fn solve(input: &Input) -> (i32, i32) {
     input
         .updates
         .iter()
-        .filter(|updates| valid_ordering(&input.rules, updates))
-        .map(|update| update[update.len() / 2])
-        .sum()
+        .map(|update| {
+            if valid_ordering(&input.rules, update) {
+                (update[update.len() / 2], 0)
+            } else {
+                (0, middle_update(&input.rules, update))
+            }
+        })
+        .fold((0, 0), |(a1, a2), (v1, v2)| (a1 + v1, a2 + v2))
 }
