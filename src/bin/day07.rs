92dfb5ba6ce0637dc8afc402e472544529b46c61
Pieter Bruyninckx <pbruyninckx@gmail.com>
2024-12-23 18:52:58 +0100

    Day 7 part 2
    Efficiency could be improved by going from right to left.
    That would take longer than waiting for this to finish running. :)

--- src/bin/day07.rs
+++ src/bin/day07.rs
@@ -1,6 +1,7 @@
 use anyhow::Error;
 use std::collections::HashSet;
 use std::fs::read_to_string;
+use std::ops::{Add, Mul};
 use std::path::Path;
 
 struct Equation {
@@ -16,13 +17,14 @@ impl Equation {
         Self { test, numbers }
     }
 
-    fn solvable(&self) -> bool {
+    fn solvable(&self, ops: &[fn(u64, u64) -> u64]) -> bool {
         self.numbers
             .iter()
             .fold(HashSet::from([0]), |acc, new| {
                 let mut ret = HashSet::new();
                 for acc_el in acc {
-                    for new_val in [acc_el + new, acc_el * new] {
+                    for op in ops {
+                        let new_val = op(acc_el, *new);
                         if new_val <= self.test {
                             ret.insert(new_val);
                         }
@@ -34,19 +36,35 @@ impl Equation {
     }
 }
 
+fn num_digits(mut val: u64) -> u32 {
+    let mut ret = 0;
+    while val > 0 {
+        ret += 1;
+        val /= 10;
+    }
+    ret
+}
+fn concatenate(a: u64, b: u64) -> u64 {
+    a * 10_u64.pow(num_digits(b)) + b
+}
+
 fn main() -> Result<(), Error> {
     let equations: Vec<_> = read_to_string(Path::new("data/input07.txt"))?
         .lines()
         .map(Equation::from_str)
         .collect();
-    println!(
-        "{}",
-        equations
-            .iter()
-            .filter(|&equation| equation.solvable())
-            .map(|equation| equation.test)
-            .sum::<u64>()
-    );
+    let ops1 = vec![u64::add, u64::mul];
+    let ops2 = vec![u64::add, u64::mul, concatenate];
+    for ops in [ops1, ops2] {
+        println!(
+            "{}",
+            equations
+                .iter()
+                .filter(|&equation| equation.solvable(&ops))
+                .map(|equation| equation.test)
+                .sum::<u64>()
+        );
+    }
 
     Ok(())
 }
