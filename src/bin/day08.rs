919ab6861e7496aca9ad832aa6c60ff428805580
Pieter Bruyninckx <pbruyninckx@gmail.com>
2024-12-24 13:22:19 +0100

    Day 8 part 2

--- src/bin/day08.rs
+++ src/bin/day08.rs
@@ -2,27 +2,28 @@ use anyhow::Error;
 use itertools::Itertools;
 use std::collections::HashMap;
 use std::fs::read_to_string;
-use std::ops::{Add, Mul, Sub};
+use std::ops::{Add, Div, Mul, Sub};
 use std::path::Path;
 
 fn main() -> Result<(), Error> {
     let map = Map::from_str(&read_to_string(Path::new("data/input08.txt"))?);
 
-    println!("{}", solve(&map));
+    [get_antinodes, get_resonant_antinodes]
+        .iter()
+        .for_each(|f| println!("{}", solve(&map, *f)));
 
     Ok(())
 }
 
-fn solve(map: &Map) -> usize {
+fn solve(map: &Map, antinode_fn: fn(&[Pos], &Map) -> Vec<Pos>) -> usize {
     map.antennas
         .iter()
-        .flat_map(|(_ch, positions)| get_antinodes(positions))
+        .flat_map(|(_ch, positions)| antinode_fn(positions, map))
         .unique()
-        .filter(|pos| map.contains(pos))
         .count()
 }
 
-fn get_antinodes(positions: &[Pos]) -> Vec<Pos> {
+fn get_antinodes(positions: &[Pos], map: &Map) -> Vec<Pos> {
     positions
         .iter()
         .combinations(2)
@@ -33,6 +34,33 @@ fn get_antinodes(positions: &[Pos]) -> Vec<Pos> {
             let diff = *p2 - *p1;
             vec![*p1 - diff, *p2 + diff]
         })
+        .filter(|pos| map.contains(pos))
+        .collect()
+}
+
+fn get_resonant_antinodes(positions: &[Pos], map: &Map) -> Vec<Pos> {
+    positions
+        .iter()
+        .combinations(2)
+        .flat_map(|pos_combo| {
+            let [p1, p2] = pos_combo[..] else {
+                panic!("Should never happen")
+            };
+            let diff = {
+                let diff = *p2 - *p1;
+                let diff_gcd = gcd(diff.x, diff.y);
+                diff / diff_gcd
+            };
+            (0..)
+                .map(move |i| *p1 + diff * i)
+                .take_while(|pos| map.contains(pos))
+                .chain(
+                    (1..)
+                        .map(move |i| *p1 - diff * i)
+                        .take_while(|pos| map.contains(pos)),
+                )
+        })
+        .filter(|pos| map.contains(pos))
         .collect()
 }
 
@@ -71,6 +99,17 @@ impl Mul<i32> for Pos {
         Pos {
             x: self.x * rhs,
             y: self.y * rhs,
+        }
+    }
+}
+
+impl Div<i32> for Pos {
+    type Output = Pos;
+
+    fn div(self, rhs: i32) -> Self::Output {
+        Pos {
+            x: self.x / rhs,
+            y: self.y / rhs,
         }
     }
 }
@@ -119,4 +158,15 @@ impl Map {
     fn contains(&self, pos: &Pos) -> bool {
         0 <= pos.x && pos.x < self.size.y && 0 <= pos.y && pos.y < self.size.y
     }
+}
+
+fn gcd(mut a: i32, mut b: i32) -> i32 {
+    a = a.abs();
+    b = b.abs();
+    while b != 0 {
+        let temp = b;
+        b = a % b;
+        a = temp;
+    }
+    a
 }
