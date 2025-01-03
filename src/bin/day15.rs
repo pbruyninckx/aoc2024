27cda769efe57bbf1a253cfdd87fd76d16d8ce69
Pieter Bruyninckx <pbruyninckx@gmail.com>
2025-01-03 21:44:17 +0100

    Day 15 part 2

--- src/bin/day15.rs
+++ src/bin/day15.rs
@@ -2,7 +2,7 @@ use crate::utils::map::{ConvertibleFromChar, Map, Pos};
 use anyhow::{anyhow, Error};
 use itertools::Itertools;
 use regex::Regex;
-use std::fmt::{Debug, Display, Formatter};
+use std::fmt::{Debug, Formatter};
 use std::fs::read_to_string;
 use std::path::Path;
 
@@ -12,7 +12,36 @@ fn main() -> Result<(), Error> {
     let (map, start, directions) = parse_input(&read_to_string(Path::new("data/input15.txt"))?)?;
 
     println!("{}", solve(map.clone(), start, &directions));
+    println!(
+        "{}",
+        solve2(map_to_two(&map), pos_to_two(&start), &directions)
+    );
     Ok(())
+}
+
+fn pos_to_two(pos: &Pos) -> Pos {
+    Pos {
+        x: 2 * pos.x,
+        y: pos.y,
+    }
+}
+
+fn map_to_two(map: &Map<Tile>) -> Map<TileTwo> {
+    let data = map
+        .data
+        .iter()
+        .map(|row| {
+            row.iter()
+                .flat_map(|t| match t {
+                    Tile::Wall => vec![TileTwo::Wall; 2],
+                    Tile::Floor => vec![TileTwo::Floor; 2],
+                    Tile::Box => vec![TileTwo::BoxLeft, TileTwo::BoxRight],
+                })
+                .collect()
+        })
+        .collect();
+    let size = pos_to_two(&map.size);
+    Map { data, size }
 }
 
 fn can_move(map: &Map<Tile>, pos: &Pos, dir: &Direction) -> bool {
@@ -48,13 +77,64 @@ fn solve(mut map: Map<Tile>, mut pos: Pos, directions: &[Direction]) -> i64 {
     score(&map)
 }
 
-fn score(map: &Map<Tile>) -> i64 {
+fn can_move2(map: &Map<TileTwo>, pos: &Pos, dir: &Direction) -> bool {
+    let next = dir.move_(pos);
+    match map[&next] {
+        TileTwo::Wall => false,
+        TileTwo::Floor => true,
+        TileTwo::BoxRight => {
+            can_move2(map, &next, dir)
+                && (dir.horizontal() || can_move2(map, &(next + Pos { x: -1, y: 0 }), dir))
+        }
+        TileTwo::BoxLeft => {
+            can_move2(map, &next, dir)
+                && (dir.horizontal() || can_move2(map, &(next + Pos { x: 1, y: 0 }), dir))
+        }
+    }
+}
+
+fn move2(map: &mut Map<TileTwo>, pos: &Pos, direction: &Direction) -> Pos {
+    let next = direction.move_(pos);
+    match map[&next] {
+        TileTwo::Wall => {
+            panic!("Method should only be called when moving is possible")
+        }
+        TileTwo::Floor => {}
+        TileTwo::BoxLeft => {
+            move2(map, &next, direction);
+            if direction.vertical() {
+                move2(map, &(next + Pos { x: 1, y: 0 }), direction);
+            }
+        }
+        TileTwo::BoxRight => {
+            move2(map, &next, direction);
+            if direction.vertical() {
+                move2(map, &(next + Pos { x: -1, y: 0 }), direction);
+            }
+        }
+    }
+    map[&next] = map[pos];
+    map[pos] = TileTwo::Floor;
+    next
+}
+
+fn solve2(mut map: Map<TileTwo>, mut pos: Pos, directions: &[Direction]) -> i64 {
+    for d in directions {
+        if can_move2(&map, &pos, d) {
+            pos = move2(&mut map, &pos, d);
+        }
+    }
+
+    score(&map)
+}
+
+fn score<T: Score + Copy>(map: &Map<T>) -> i64 {
     map.data
         .iter()
         .enumerate()
         .flat_map(|(y, v)| {
             v.iter().enumerate().map(move |(x, &tile)| {
-                if tile == Tile::Box {
+                if tile.count_for_score() {
                     (100 * y + x) as i64
                 } else {
                     0
@@ -116,12 +196,50 @@ enum Tile {
     Box,
 }
 
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum TileTwo {
+    Wall,
+    Floor,
+    BoxLeft,
+    BoxRight,
+}
+
+trait Score {
+    fn count_for_score(self) -> bool;
+}
+
+impl Score for Tile {
+    fn count_for_score(self) -> bool {
+        self == Tile::Box
+    }
+}
+
+impl Score for TileTwo {
+    fn count_for_score(self) -> bool {
+        self == TileTwo::BoxLeft
+    }
+}
+
 impl Debug for Tile {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         let c = match self {
             Tile::Wall => '#',
             Tile::Floor => '.',
             Tile::Box => 'O',
+        };
+
+        write!(f, "{}", c)?;
+        Ok(())
+    }
+}
+
+impl Debug for TileTwo {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let c = match self {
+            TileTwo::Wall => '#',
+            TileTwo::Floor => '.',
+            TileTwo::BoxLeft => '[',
+            TileTwo::BoxRight => ']',
         };
 
         write!(f, "{}", c)?;
@@ -167,5 +285,13 @@ impl Direction {
                 y: pos.y + 1,
             },
         }
+    }
+
+    fn vertical(self) -> bool {
+        self == Direction::Up || self == Direction::Down
+    }
+
+    fn horizontal(self) -> bool {
+        !self.vertical()
     }
 }
