47b42e393a5323d95dfdf7c1ecaad081a95ae48d
Pieter Bruyninckx <pbruyninckx@gmail.com>
2025-01-05 13:25:49 +0100

    Day 16 part 2
    Note: There's a bug when two equal length paths converge at the end
    position.

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -1,20 +1,22 @@
 use crate::utils::map::{ConvertibleFromChar, Map, Pos};
 use anyhow::Error;
+use itertools::Itertools;
 use std::cmp::Ordering;
-use std::collections::{BinaryHeap, HashSet};
+use std::collections::{BinaryHeap, HashMap, HashSet};
 use std::fs::read_to_string;
 use std::path::Path;
 
-mod utils;
+pub mod utils;
 
 fn main() -> Result<(), Error> {
     let (map, start, end) = parse_input(&read_to_string(Path::new("data/input16.txt"))?)?;
 
-    println!("{}", solve(&map, start, end));
+    let (result1, result2) = solve(&map, start, end);
+    println!("{}\n{}", result1, result2);
     Ok(())
 }
 
-fn solve(map: &Map<Tile>, start: Pos, end: Pos) -> i64 {
+fn solve(map: &Map<Tile>, start: Pos, end: Pos) -> (i64, i64) {
     let mut heap = BinaryHeap::new();
     let start_state = State {
         pos: start,
@@ -24,34 +26,37 @@ fn solve(map: &Map<Tile>, start: Pos, end: Pos) -> i64 {
         state: start_state,
         score: 0,
     });
-    let mut seen = HashSet::new();
+    let mut seen = HashMap::new();
 
     while let Some(scored_state) = heap.pop() {
+        seen.insert(scored_state.state, scored_state.score);
         if scored_state.state.pos == end {
-            return scored_state.score;
+            let best_path_tiles = compute_best_path_tiles(&seen, end);
+            return (scored_state.score, best_path_tiles);
         }
-        seen.insert(scored_state.state);
 
+        // Move forward
         {
             let next_position = scored_state.state.direction.move_(&scored_state.state.pos);
             let next_state = State {
                 pos: next_position,
                 direction: scored_state.state.direction,
             };
-            if map[&next_position] == Tile::Floor && !seen.contains(&next_state) {
+            if map[&next_position] == Tile::Floor && !seen.contains_key(&next_state) {
                 heap.push(ScoredState {
                     score: scored_state.score + 1,
                     state: next_state,
                 })
             }
         }
+        // Turn (and move)
         for next_direction in &scored_state.state.direction.neighbours() {
             let next_pos = next_direction.move_(&scored_state.state.pos);
             let next_state = State {
                 pos: next_pos,
                 direction: *next_direction,
             };
-            if map[&next_pos] == Tile::Floor && !seen.contains(&next_state) {
+            if map[&next_pos] == Tile::Floor && !seen.contains_key(&next_state) {
                 heap.push(ScoredState {
                     state: next_state,
                     score: scored_state.score + 1001,
@@ -59,7 +64,7 @@ fn solve(map: &Map<Tile>, start: Pos, end: Pos) -> i64 {
             }
         }
 
-        while seen.contains(
+        while seen.contains_key(
             &heap
                 .peek()
                 .expect("loop should break before heap runs empty")
@@ -69,6 +74,43 @@ fn solve(map: &Map<Tile>, start: Pos, end: Pos) -> i64 {
         }
     }
     unreachable!("We should reach the 'E' position")
+}
+
+fn compute_best_path_tiles(scores: &HashMap<State, i64>, end: Pos) -> i64 {
+    let mut seen = HashSet::new();
+    let mut stack = Vec::new();
+    for d in [Direction::Up, Direction::Right] {
+        let end_state = State {
+            pos: end,
+            direction: d,
+        };
+        if scores.contains_key(&end_state) {
+            stack.push(end_state);
+        }
+    }
+    assert!(!stack.is_empty());
+    while let Some(state) = stack.pop() {
+        seen.insert(state);
+        let previous_position = state.direction.invert().move_(&state.pos);
+        let previous_directions = {
+            let mut directions = state.direction.neighbours();
+            directions.push(state.direction);
+            directions
+        };
+        for (previous_direction, diff) in previous_directions.iter().zip([1001, 1001, 1].iter()) {
+            let previous_state = State {
+                pos: previous_position,
+                direction: *previous_direction,
+            };
+            if let Some(score) = scores.get(&previous_state) {
+                if *score == scores[&state] - diff {
+                    stack.push(previous_state);
+                }
+            }
+        }
+    }
+
+    seen.iter().map(|state| state.pos).unique().count() as i64
 }
 
 fn parse_input(input: &str) -> Result<(Map<Tile>, Pos, Pos), Error> {
@@ -125,6 +167,15 @@ impl Direction {
             Direction::Left | Direction::Right => {
                 vec![Direction::Up, Direction::Down]
             }
+        }
+    }
+
+    fn invert(&self) -> Direction {
+        match self {
+            Direction::Up => Direction::Down,
+            Direction::Down => Direction::Up,
+            Direction::Left => Direction::Right,
+            Direction::Right => Direction::Left,
         }
     }
 
